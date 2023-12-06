6c7388bceba0bd1695a17197a52ad26648fa7634
Redomine <romankaev@gmail.com>
2023-12-06 17:03:43 +0300

    Задачи по первой практической

---
+++ Practice 1.4/Task2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
