2fd46d5a4f38e8a31308e0933ae24a055cd47284
Redomine <romankaev@gmail.com>
2024-01-09 17:57:06 +0300

    5.5 Практическая работа

---
+++ Practice 5.5/Task 2/Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static string SplitAndReverseText(string text)
+        {
+            string[] WordArray = text.Split(' ');
+            return Reverse(WordArray);
+        }
+
+        static string Reverse(string[] WordArray)
+        {
+            string ResultString = "";
+            foreach (string word in WordArray.Reverse())
+            {
+                ResultString += word + " ";
+            }
+            return ResultString;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Введите предложение:");
+            string InputData = Console.ReadLine();
+            Console.WriteLine(SplitAndReverseText(InputData));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice 5.5/Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+
+        static void PrintWords(string[] text)
+        {
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string InputData = Console.ReadLine();
+            PrintWords(SplitText(InputData));
+            Console.ReadKey();
+        }
+    }
+}
