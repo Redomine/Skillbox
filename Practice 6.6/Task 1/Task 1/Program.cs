82c97da5dcbc05b877d33fb6bc0e69fdd61407a7
Redomine <romankaev@gmail.com>
2024-01-15 12:13:43 +0300

    6.6 Практическая работа

---
+++ Practice 6.6/Task 1/Task 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1
+{
+    internal class Program
+    {
+
+        static string GetConsoleWriteLine(string message)
+        {
+            string data = "#";
+            Console.WriteLine(message);
+            data += Console.ReadLine();
+            return data;
+        }
+
+        static string CreateEmployeeData()
+        {
+            string id = GetConsoleWriteLine("Введите ID сотрудника");
+
+            string fullName = GetConsoleWriteLine("Введите полное имя сотрудника");
+
+            string height = GetConsoleWriteLine("Введите рост");
+            string birthDate = GetConsoleWriteLine("Введите дату рождения");
+            string birthCity = GetConsoleWriteLine("Введите место рождение");
+
+            string employeeData = id + "#" + DateTime.Now + fullName + height + birthDate + birthCity;
+
+            return employeeData;
+        }
+
+        static void ReadData()
+        {
+            string line = "";
+            using (StreamReader sr = new StreamReader("EmployeesData.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line.Replace("#", " "));
+                }
+            }
+        }
+
+        static void WriteData()
+        {
+            using (StreamWriter sw = new StreamWriter("EmployeesData.txt", append: true))
+            {
+
+                sw.WriteLine(CreateEmployeeData());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1, чтоб вывести данные на экран, введите 2, чтобы дополнить данные.");
+            string workMode = Console.ReadLine();
+
+            if (workMode == "2")
+            {
+                WriteData();
+            }
+            else
+            {
+                ReadData();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
