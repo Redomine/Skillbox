4ae2ed8eceb3fd65a03e2da0deb58b3a2149adca
Redomine <52177952+Redomine@users.noreply.github.com>
2024-05-13 21:49:21 +0300

    Update

--- Practice 11.7/Task 1/Task 1/Classes/Repository.cs
+++ Practice 11.7/Task 1/Task 1/Classes/Repository.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Task_1.Classes;
 using Task_1.Classes.Task_1_3;
 using Task_1.Interfaces;
 using Task_1.Windows;
@@ -76,96 +77,116 @@ namespace Task_1.Model
             }
         }
 
+
+        /// <summary>
+        /// Проверяет параметры на пустоту и не пустые вставляет в клиента, возвращает новое значение имени изменения
+        /// </summary>
+        private string ReplacePartOfClientsDb(string nameOfChanged, Client dbClient, Dictionary<string, string> dbKeys)
+        {
+            foreach (string key in dbKeys.Keys)
+            {
+                if (dbKeys[key] != "")
+                {
+                    nameOfChanged += key + " ";
+                }
+            }
+            if (dbKeys["FirstName"] != "") { dbClient.FirstName = dbKeys["FirstName"]; }
+            if (dbKeys["SecondName"] != "") { dbClient.SecondName = dbKeys["SecondName"]; }
+            if (dbKeys["SurName"] != "") { dbClient.SurName = dbKeys["SurName"]; }
+            if (dbKeys["PhoneNumber"] != "") { dbClient.PhoneNumber = dbKeys["PhoneNumber"]; }
+            if (dbKeys["PassportNumber"] != "") { dbClient.PassportNumber = dbKeys["PassportNumber"]; }
+
+            return nameOfChanged;
+        }
+
+        /// <summary>
+        /// Ищет клиента по айди или возвращает null
+        /// </summary>
+        private Client FindClient(ObservableCollection<Client> ClientsDb, int id)
+        {
+            foreach (Client dbClient in ClientsDb)
+            {
+                if (dbClient.Id == id)
+                {
+                    return dbClient;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Редактирует только номер телефона
+        /// </summary>
+        private void EditPhone(ChangePhone newDialog, Client client, IEmployee worker)
+        {
+            string phoneNumber = newDialog.PhoneNumber.Text;
+            string nameOfChanged = "";
+            Client dbClient = FindClient(ClientsDb, client.Id);
+            
+            if (phoneNumber != "")
+            {
+                dbClient.PhoneNumber = phoneNumber;
+                nameOfChanged += "PhoneNumber";
+                dbClient.AutorOfChanges = worker.WorkerName;
+                dbClient.ChangesDate = DateTime.Now;
+                dbClient.NameOfChangedData = nameOfChanged;
+                dbClient.TypeOfChanges = "Изменение параметров";
+            }
+
+            SerializeClientsToJson(ClientsDb);
+            ClientsDb = DeserializeClientsFromJson();
+        }
+
+        /// <summary>
+        /// Редактирует всю строку данных из ListView
+        /// </summary>
+        private void EditRow(EditClient newDialog, Client client, IEmployee worker)
+        {
+            string firstName = newDialog.FirstName.Text;
+            string secondName = newDialog.SecondName.Text;
+            string surName = newDialog.SurName.Text;
+            string phoneNumber = newDialog.PhoneNumber.Text;
+            string passportNumber = newDialog.PassportNumber.Text;
+            string nameOfChanged = "";
+            Dictionary<string, string> paraNamesParaValues = new Dictionary<string, string>()
+                            {
+                                { "FirstName", firstName },
+                                { "SecondName", secondName },
+                                { "SurName", surName },
+                                { "PhoneNumber", phoneNumber },
+                                { "PassportNumber", passportNumber },
+                            };
+            Client dbClient = FindClient(ClientsDb, client.Id);
+            nameOfChanged = ReplacePartOfClientsDb(nameOfChanged, dbClient, paraNamesParaValues);
+            if (nameOfChanged != "")
+            {
+                dbClient.AutorOfChanges = worker.WorkerName;
+                dbClient.ChangesDate = DateTime.Now;
+                dbClient.NameOfChangedData = nameOfChanged;
+                dbClient.TypeOfChanges = "Изменение параметров";
+            }
+
+            SerializeClientsToJson(ClientsDb);
+            ClientsDb = DeserializeClientsFromJson();
+        }
+
         public void EditClient(MainWindow mainWindow, Client client, IEmployee worker)
         {
-            string nameOfChanged = "";
-            
-
             if (client != null) 
             {
-                MessageBox.Show(client.Id.ToString());
-                string phoneNumber;
                 if (worker.LevelOfAccess == "0")
                 {
                     ChangePhone newDialog = new ChangePhone();
                     NewWindowAsDialog(mainWindow, newDialog);
-                    phoneNumber = newDialog.PhoneNumber.Text;
-
-                    foreach (Client dbClient in ClientsDb)
-                    {
-                        if (dbClient.Id == client.Id)
-                        {
-                            {
-                                dbClient.PhoneNumber = phoneNumber;
-                                nameOfChanged += newDialog.PhoneNumber.Name + " ";
-                                dbClient.AutorOfChanges = worker.WorkerName;
-                                dbClient.ChangesDate = DateTime.Now;
-                                dbClient.NameOfChangedData = nameOfChanged;
-                                dbClient.TypeOfChanges = "Изменение параметров";
-                            }
-                        }
-                    }
-                    SerializeClientsToJson(ClientsDb);
-                    ClientsDb = DeserializeClientsFromJson();
-
+                    EditPhone(newDialog, client, worker);
                 }
                 else
                 {
                     EditClient newDialog = new EditClient();
                     NewWindowAsDialog(mainWindow, newDialog);
-                    string firstName = newDialog.FirstName.Text;
-                    string secondName = newDialog.SecondName.Text;
-                    string surName = newDialog.SurName.Text;
-                    phoneNumber = newDialog.PhoneNumber.Text;
-                    string passportNumber = newDialog.PassportNumber.Text;
-                    
-
-                    foreach (Client dbClient in ClientsDb)
-                    {
-                        if (dbClient.Id == client.Id)
-                        {
-                            if (firstName != "")
-                            {
-                                
-                                dbClient.FirstName = firstName;
-                                nameOfChanged += newDialog.FirstName.Name + " ";
-                            }
-                            if (secondName != "")
-                            {
-                                dbClient.SecondName = secondName;
-                                nameOfChanged += newDialog.SecondName.Name + " ";
-                            }
-                            if (surName != "")
-                            {
-                                dbClient.SurName = surName;
-                                nameOfChanged += newDialog.SurName.Name + " ";
-                            }
-                            if (phoneNumber != "")
-                            {
-                                dbClient.PhoneNumber = phoneNumber;
-                                nameOfChanged += newDialog.PhoneNumber.Name + " ";
-                            }
-                            if (passportNumber != "")
-                            {
-                                dbClient.PassportNumber = passportNumber;
-                                nameOfChanged = newDialog.PassportNumber.Name + " ";
-                            }
-                            if (nameOfChanged != "")
-                            {
-                                dbClient.AutorOfChanges = worker.WorkerName;
-                                dbClient.ChangesDate = DateTime.Now;
-                                dbClient.NameOfChangedData = nameOfChanged;
-                                dbClient.TypeOfChanges = "Изменение параметров";
-                            }
-                        }
-                    }
-                    SerializeClientsToJson(ClientsDb);
-                    ClientsDb = DeserializeClientsFromJson();
+                    EditRow(newDialog, client, worker);
                 }
-
-
             }
-            
             mainWindow.lvClients.ItemsSource = ClientsDb;
             mainWindow.lvClients.Items.Refresh();
         }
@@ -181,7 +202,7 @@ namespace Task_1.Model
             string phoneNumber = newDialog.PhoneNumber.Text;
             string passportNumber = newDialog.PassportNumber.Text;
 
-            if ((firstName == "" || secondName == "" ||  surName == "" || phoneNumber == "" || passportNumber == "") & newDialog.editable)
+            if (((firstName ?? secondName ?? surName ?? phoneNumber ?? passportNumber) == "") & newDialog.editable)
             {
                 MessageBox.Show("Должны быть заполнены все поля");
             }
